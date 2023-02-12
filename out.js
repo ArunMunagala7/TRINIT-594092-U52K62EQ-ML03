71cbf14486c08ef3adfa94c5b077c9bcae769936
Arun Munagala <99646583+ArunMunagala7@users.noreply.github.com>
2023-02-12 07:17:48 +0530

    Add files via upload

---
+++ out.js
@@ -0,0 +1,11 @@
+$(".soil").click(function(){
+    $(".send_soil").removeClass("notvisi");
+    $(".send_info").addClass("notvisi");
+    console.log("Rohan has a crush on Avni");
+});
+
+$(".market").click(function(){
+    $(".send_info").removeClass("notvisi");
+    $(".send_soil").addClass("notvisi");
+    console.log("Arun wants To have gay sex with harshit");
+});
